31bd8dfdf27f26b8c63ec62bf9a000c830405915
Sean McElholm <seanmcelholm@gmail.com>
2019-05-27 23:14:28 +0100

    Add primiative mini-map and a friend NPC

---
+++ Assets/Scripts/Misc/DayNightCycle.cs
@@ -0,0 +1,26 @@
+﻿//
+// 	Copyright (C) 2019 Outlaw Games Studio. All Rights Reserved.
+//
+// 	This document is the property of Outlaw Games Studio.
+// 	It is considered confidential and proprietary.
+//
+// 	This document may not be reproduced or transmitted in any form
+// 	without the consent of Outlaw Games Studio.
+//
+using UnityEngine;
+using System.Collections;
+
+public class NewMonoBehaviour : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
