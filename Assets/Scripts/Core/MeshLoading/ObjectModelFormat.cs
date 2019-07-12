8e8c31a55e0dc30dc6aaa6411777be1716ca2022
Sean McElholm <seanmcelholm@gmail.com>
2019-07-12 18:01:35 +0100

    Added foundation for mesh loading

--- Assets/Scripts/Core/MeshLoading/ObjectModelFormat.cs
+++ Assets/Scripts/Core/MeshLoading/ObjectModelFormat.cs
@@ -7,21 +7,41 @@
 // 	This document may not be reproduced or transmitted in any form
 // 	without the consent of Outlaw Games Studio.
 //
+
+using System;
 using UnityEngine;
 using System.IO;
+using System.IO.Compression;
 
-public class ObjectModelFormat : MonoBehaviour
+public class ObjectModelFormat : Singleton<ObjectModelFormat>
 {
-    public static readonly string FILE_EXT = ".omf";
+    private static readonly string FileExt = ".omf";
+
+    private void Start()
+    {
+        LoadObjectFile("Test.omf");
+    }
 
     public void LoadObjectFile(string objectFile)
     {
-        if(!File.Exists(objectFile))
+        if (!objectFile.EndsWith(FileExt))
+        {
+            objectFile += FileExt;
+        }
+
+        string filePath = Path.Combine(Application.streamingAssetsPath, "Models", objectFile);
+        if(!File.Exists(filePath))
         {
             Debug.LogError($"Object Model Format file {objectFile} does not exist on the local disk.");
             return;
         }
 
-
+        using (ZipArchive zipArchive = ZipFile.OpenRead(filePath))
+        {
+            foreach (var zip in zipArchive.Entries)
+            {
+                Debug.Log($"Fullname: {zip.FullName}");
+            }
+        }
     }
 }
