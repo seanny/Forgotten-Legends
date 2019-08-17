ef5fe817e478b087204cd6177442c1b341b1c656
Sean McElholm <seanmcelholm@gmail.com>
2019-08-18 00:43:20 +0100

    Add the ability to write OGSD files.

--- Assets/Scripts/Core/DataFormat/OGSDReader.cs
+++ Assets/Scripts/Core/DataFormat/OGSDReader.cs
@@ -1,10 +1,15 @@
 using System;
 using System.IO;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace Core.DataFormat
 {
+    /// <summary>
+    /// Class for reading and writing OGSD files
+    /// OGSD = Outlaw Games Studio Data
+    /// </summary>
     public class OGSDReader
     {
         public OGSDFile dataFile
@@ -26,13 +31,41 @@ namespace Core.DataFormat
                 Debug.LogError($"[OGSDReader]: {Path.GetFileName(filePath)} does not exist.");
                 return;
             }
-            
+
             BinaryFormatter binaryFormatter = new BinaryFormatter();
             FileStream fileStream = File.Open(filePath, FileMode.Open);
             m_DataFile = (OGSDFile) binaryFormatter.Deserialize(fileStream);
             fileStream.Close();
         }
 
+        public void Write(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                Debug.LogWarning($"Overwriting file {filePath}...");
+                File.Delete(filePath);
+            }
+            else
+            {
+                Debug.Log($"Writing file {filePath}...");
+            }
+
+            FileStream fileStream = File.Open(filePath, FileMode.Create);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            try
+            {
+                binaryFormatter.Serialize(fileStream, dataFile);
+            }
+            catch (SerializationException error)
+            {
+                Debug.LogError($"[OGSDReader]: {Path.GetFileName(filePath)} cannot be serialised: {error.Message}");
+            }
+            finally
+            {
+                fileStream.Close();
+            }
+        }
+        
         public void Close()
         {
             m_DataFile = null;
