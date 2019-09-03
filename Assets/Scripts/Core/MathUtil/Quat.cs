a27349c9d9089f7c320e44858847d4bd7b772808
Sean McElholm <seanmcelholm@gmail.com>
2019-09-03 13:38:38 +0100

    Add a serializable quaternion.

---
+++ Assets/Scripts/Core/MathUtil/Quat.cs
@@ -0,0 +1,39 @@
+//
+// 	Copyright (C) 2019 Outlaw Games Studio. All Rights Reserved.
+//
+// 	This document is the property of Outlaw Games Studio.
+// 	It is considered confidential and proprietary.
+//
+// 	This document may not be reproduced or transmitted in any form
+// 	without the consent of Outlaw Games Studio.
+//
+using System;
+using UnityEngine;
+
+namespace Core.MathUtil
+{
+    [Serializable]
+    public class Quat
+    {
+        public float x { get; private set; }
+        public float y { get; private set; }
+        public float z { get; private set; }
+        public float w { get; private set; }
+        
+        public Quat(float x, float y, float z, float w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+        
+        public Quat(Quaternion quaternion)
+        {
+            this.x = quaternion.x;
+            this.y = quaternion.y;
+            this.z = quaternion.z;
+            this.w = quaternion.w;
+        }
+    }
+}
